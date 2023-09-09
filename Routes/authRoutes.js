4aa070048663ada5fc6c4545b71ce73a0539c352
Ashraf-Khalifa <ashrafrkhalifa@gmail.com>
2023-09-09 22:26:10 +0300

    done

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const AuthController = require("../controllers/AuthController");
+const AuthController = require('../Controllers/AuthController');
 
 // Routes for authentication
 router.post("/sendotp", AuthController.sendOTP);
